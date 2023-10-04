2a6799be1005b4ae75710e348c60390bf8975772
Isidro-Olivares <isidro.olivares4159@alumnos.udg.mx>
2023-10-04 16:38:31 -0600

    inicio de sesion

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/plantilla', function () {
+    return view('plantilla');
+});
+
+
 //Rout::get('participante/pdf')
 
 Route::resource('participante', ParticipanteController::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
